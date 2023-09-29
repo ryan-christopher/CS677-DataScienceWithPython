5cfe885c4433f9f13be5d452058388fab57b26f1
ryan-christopher <rechristopher99@gmail.com>
2023-09-29 01:20:14 -0400

    edited file and func names, label sequence correct

--- assignment2/predict.py
+++ assignment2/predict.py
@@ -8,18 +8,46 @@ Assignment 2 Question 2
 
 '''
 import pandas as pd
-from true_value import getTable
+from true_label import getTable
+global wVal
 
-stock_data = getTable('COST')
-print(type(stock_data))
+cost_stock_data = getTable('COST')
 
-def predictWSequence(df, w):
-    wList = [val for val in range(w + 1)]
-    shiftValues = df['True Value'].shift(periods=wList)
-    for i in range(1, w + 1):
-        df['Day-' + str(i)] = shiftValues['True Value_' + str(i)]
-    print(shiftValues)
+# create global level string to store sequence of +'s and -'s
+# (global level works best as it is mainly 
+# being used with an apply method)
+true_labels = ""
+
+# gatherTrainingLabels is a helper function that takes a given
+# day's True Label and adds it to the string true_labels
+def gatherTrainingLabels(df):
+    global true_labels
+    if int(df['Year']) < 2019:
+        true_labels += df['True Label']
+
+
+def generateLabelSequence(df):
+    global wVal
+    for i in range(1, wVal + 1):
+        df["Past" + str(i)] = "*" * i
+    if int(df['Year']) > 2018:
+        sequence = ""
+        for i in range(1, wVal + 1):
+            sequence += df['Day-' + str(i)]
+            df["Past" + str(i)] = sequence
     return df
 
-shift = predictWSequence(stock_data, 4)
-print(shift)
+def predictWSequence(df, w):
+    global true_labels
+    global wVal
+    true_labels, wVal = "", w
+    df.apply(gatherTrainingLabels, axis = 1)
+    wList = [val for val in range(w + 1)]
+    shiftValues = df['True Label'].shift(periods=wList)
+    for i in range(1, w + 1):
+        df['Day-' + str(i)] = shiftValues['True Label_' + str(i)]
+    df = df.apply(generateLabelSequence, axis = 1)
+    print(df)
+    return df
+
+cost_stock_data = predictWSequence(cost_stock_data, 4)
--- assignment2/true_label.py
+++ assignment2/true_label.py
@@ -6,39 +6,41 @@ Assignment 2 Question 1
 
 =======Description of Problem=======
 Taking the csv of my stock and SPY, create a dataframe with that info.
-Then, add in a column of the "True Value" of the stock as a + or -,
-and write a function that can determine the likelihood of a day's "True Value"
+Then, add in a column of the "True Label" of the stock as a + or -,
+and write a function that can determine the likelihood of a day's "True Label"
 based off the prior sequence of up and down days.
 '''
 import pandas as pd
 
 # ========== Part 1 ==========
-# determineTrueValue assigns the True Value column to + if the 
+# determine TrueLabel assigns the True Label column to + if the 
 # day's return is positive, and - if the day's return is negative
-def determineTrueValue(df):
+def determineTrueLabel(df):
     if float(df['Return']) >= 0:
-        df['True Value'] = '+'
+        df['True Label'] = '+'
     else:
-        df['True Value'] = '-'
+        df['True Label'] = '-'
     return df
 
 # use the apply method on the dataframe to assign the correct 
-# True Value for each day
+# True Label for each day
 def getTable(stock):
     # read CSV data and store into dataframe called stock_data
     stock_data = pd.read_csv('assignment2/stock_data/' + stock + '.csv')
 
     # remove extra info that we will not need to use
-    stock_data = stock_data.drop(['Week_Number', 'Year_Week', 'Open', 'High', 'Low',
-                  'Close', 'Volume', 'Adj Close', 'Short_MA', 'Long_MA'], 
+    stock_data = stock_data.drop(['Date', 'Week_Number', 'Year_Week', 'Open', 
+                'High', 'Low', 'Close', 'Volume', 'Adj Close', 'Short_MA', 'Long_MA'], 
                   axis = 1)
     
-    stock_data = stock_data.apply(determineTrueValue, axis = 1)
+    stock_data = stock_data.apply(determineTrueLabel, axis = 1)
 
     return stock_data
 
-stock_data = getTable('COST')
-# print(stock_data)
+cost_stock_data = getTable('COST')
+spy_stock_data = getTable('SPY')
+# print(cost_stock_data)
+# print(spy_stock_data)
 
 
 # ========== Part 2 ==========
@@ -50,22 +52,23 @@ stock_data = getTable('COST')
 # being used with an apply method)
 true_labels = ""
 
-# gatherValues is a helper function that takes a given
-# day's True Value and adds it to the string true_labels
-def gatherValues(df):
+# gatherTrainingLabels is a helper function that takes a given
+# day's True Label and adds it to the string true_labels
+def gatherTrainingLabels(df):
     global true_labels
     if int(df['Year']) < 2019:
-        true_labels += df['True Value']
+        true_labels += df['True Label']
 
-# apply the gatherValues function to the dataframe given 
+# apply the gatherTrainingLabels function to the dataframe given 
 # as a parameter, and return the probability of an up day
 def upDayProb(df):
     global true_labels
     true_labels = ""
-    df.apply(gatherValues, axis = 1)
+    df.apply(gatherTrainingLabels, axis = 1)
     return true_labels.count("+") / len(true_labels)
 
-# print(upDayProb(stock_data))
+# print(upDayProb(cost_stock_data))
+# print(upDayProb(spy_stock_data))
 
 
 # ========== Part 3 and 4 ==========
@@ -76,8 +79,8 @@ def kSequenceProbability(df, sequenceVal, targetVal, kVal):
     global true_labels
     true_labels, probabilities = "", []
 
-    # generate string of True Values 
-    df.apply(gatherValues, axis = 1)
+    # generate string of True Labels 
+    df.apply(gatherTrainingLabels, axis = 1)
 
     # calculate probability of each value for k, then append 
     # result to the list probabilities 
@@ -90,7 +93,9 @@ def kSequenceProbability(df, sequenceVal, targetVal, kVal):
     return probabilities
 
 # probability of up day after k consecutive down days
-# print(kSequenceProbability(stock_data, '-', '+', 3))
+# print(kSequenceProbability(cost_stock_data, '-', '+', 3))
+# print(kSequenceProbability(spy_stock_data, '-', '+', 3))
 
 # probability of down day after k consecutive up days
-# print(kSequenceProbability(stock_data, '+', '-', 3))
+# print(kSequenceProbability(cost_stock_data, '+', '-', 3))
+# print(kSequenceProbability(spy_stock_data, '+', '-', 3))
