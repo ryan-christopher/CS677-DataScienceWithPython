df6dc67157298aa378580d684e68097c90698602
ryan-christopher <rechristopher99@gmail.com>
2023-10-31 19:41:10 -0400

    added plots

--- assignment4/question1.py
+++ assignment4/question1.py
@@ -11,6 +11,9 @@ Then for each dataset, construct the visual representations of correponding
 correlation matrices M0 and M1, and examine the correlation matrix plots.
 '''
 import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 # getHeartData reads the csv of heat failure data, extracts two dataframes 
 # with the 4 features mentioned above, and generates plots for both
@@ -22,19 +25,24 @@ def getHeartData():
 
     # remove extra columns
     heart_data = heart_data.drop(['anaemia', 'diabetes', 'ejection_fraction', 'age',
-                                  'high_blood_pressure', 'sex', 'smoking', 'time'], axis = 1)
+                                'high_blood_pressure', 'sex', 'smoking', 'time'],
+                                axis = 1)
     
     # reorder columns 
     heart_data = heart_data[['creatinine_phosphokinase', 'serum_creatinine', 
                              'serum_sodium', 'platelets', 'DEATH_EVENT']]
 
     # separate into 2 classes based on DEATH_EVENT
-    class_0 = heart_data.where(heart_data['DEATH_EVENT'] == 0).dropna()
-    class_1 = heart_data.where(heart_data['DEATH_EVENT'] == 1).dropna()
+    class_0 = heart_data.where(heart_data['DEATH_EVENT'] == 0).dropna().drop('DEATH_EVENT', axis = 1)
+    class_1 = heart_data.where(heart_data['DEATH_EVENT'] == 1).dropna().drop('DEATH_EVENT', axis = 1)
 
+    print(class_0)
     print(class_1)
 
-    #print(heart_data)
+    # use seaborn pairplot to generate plot for each class
+    # sns.pairplot(class_0)
+    # sns.pairplot(class_1)
+    # plt.show()
 
 
 getHeartData()
